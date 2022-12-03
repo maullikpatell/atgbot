383a6d0d163ea0096ce6742fc3b0da1cc601b1fb
technicalatg <119067757+technicalatg@users.noreply.github.com>
2022-12-03 12:51:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/maullikpatell/DTG-linkszad-bot.git /DTG-linkszad-bot
+  git clone https://github.com/technicalatg/atgbot.git /atgbot
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /DTG-linkszad-bot
+  git clone $SOURCE_CODE /atgbot
 fi
-cd /DTG-linkszad-bot
+cd /atgbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 -m main
