9006b4b3a533b2aeb9b8ea07a0b993f2b6206fdd
technicalatg <119067757+technicalatg@users.noreply.github.com>
2022-12-19 03:46:15 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -68,9 +68,9 @@ Current Method: {method}
     
 Methods Available:
 
-~ `mdlink` - Change all the links of the post to your MDisk account first and then short to MAKESHORT.IN link.
+~ `mdlink` - Change all the links of the post to your MDisk account first and then short to ATG link.
 
-~ `shortener` - Short all the links of the post to MAKESHORT.IN link directly.
+~ `shortener` - Short all the links of the post to ATG link directly.
 
 To change method, choose it from the following options:
 """
